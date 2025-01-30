6669017c37508a7c8340e635826e1229f27a938c
Chris Ward <665123+cw6365@users.noreply.github.com>
2025-01-30 21:51:42 +0000

    updated workflow section

--- js/main.js
+++ js/main.js
@@ -1,24 +1,24 @@
 AOS.init({
   once: true,
-  disable: 'phone',
+  disable: "phone",
   duration: 500,
-  easing: 'ease-out-cubic',
+  easing: "ease-out-cubic",
 });
 
-const carouselEl = document.querySelectorAll('.carousel');
+const carouselEl = document.querySelectorAll(".carousel");
 if (carouselEl.length > 0) {
-  const carousel = new Swiper('.carousel', {
+  const carousel = new Swiper(".carousel", {
     breakpoints: {
       320: {
-        slidesPerView: 1
+        slidesPerView: 1,
       },
       640: {
-        slidesPerView: 2
+        slidesPerView: 2,
       },
       1024: {
-        slidesPerView: 3
-      }      
-    },  
+        slidesPerView: 3,
+      },
+    },
     grabCursor: true,
     loop: false,
     centeredSlides: false,
@@ -28,8 +28,23 @@ if (carouselEl.length > 0) {
       delay: 7000,
     },
     navigation: {
-      nextEl: '.carousel-next',
-      prevEl: '.carousel-prev',
+      nextEl: ".carousel-next",
+      prevEl: ".carousel-prev",
     },
   });
 }
+// Wrap every letter in a span
+var textWrapper = document.querySelector(".ml3");
+textWrapper.innerHTML = textWrapper.textContent.replace(
+  /\S/g,
+  "<span class='letter'>$&</span>"
+);
+
+anime.timeline().add({
+  targets: ".ml3 .letter",
+  opacity: [0, 1],
+  easing: "easeInOutQuad",
+  duration: 450,
+  delay: (el, i) => 100 * (i + 1),
+});
+
